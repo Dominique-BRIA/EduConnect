2a325395f61a777d200be51d66f46c3639b0adbb
Dominique-BRIA <d.bria00@gmail.com>
2026-04-22 19:39:04 +0200

    Delete init.sql

--- init.sql
+++
@@ -1,9 +0,0 @@
--- Script exécuté automatiquement au premier démarrage du conteneur MySQL
--- Les tables sont créées par Hibernate (ddl-auto: update), ce script
--- sert uniquement à configurer le charset et les droits.
-
-ALTER DATABASE hackaton_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Accorder tous les droits à l'utilisateur applicatif
-GRANT ALL PRIVILEGES ON hackaton_db.* TO 'educonnect'@'%';
-FLUSH PRIVILEGES;
